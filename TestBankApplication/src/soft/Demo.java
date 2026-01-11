fe8f48bd959a3d3ded75653e97c8f677e6bf7914
vaishnaviaginkar <vaishnavi98aginkar@gmail.com>
2026-01-11 10:10:56 -0800

    changes in demo file

--- TestBankApplication/src/soft/Demo.java
+++ TestBankApplication/src/soft/Demo.java
@@ -5,7 +5,8 @@ public class Demo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.print("Welcome to SBI Bank");
+		System.out.println("Welcome to SBI Bank");
+		System.out.println("Welcome to github");
 
 	}
 
